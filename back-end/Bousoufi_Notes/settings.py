ef7dac932021c28e1b80a7023d9b99a4d3a30178
Mohamed-Bousoufi <lahmami96@gmail.com>
2024-12-18 23:07:02 +0100

    config settings.py

--- back-end/Bousoufi_Notes/settings.py
+++ back-end/Bousoufi_Notes/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+
+
+PORT = int(os.environ.get('PORT', 8000))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
