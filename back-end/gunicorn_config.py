c68fc7cd4e916712073d06dd26e54938e998e8e4
Mohamed-Bousoufi <lahmami96@gmail.com>
2024-12-18 23:49:00 +0100

    config settings.py

--- back-end/gunicorn_config.py
+++ back-end/gunicorn_config.py
@@ -1,6 +1,6 @@
-# import os
+import os
 
-# bind = f"0.0.0.0:{os.environ.get('PORT', '8000')}"
-# workers = 4
-# threads = 4
-# timeout = 120
+bind = f"0.0.0.0:{os.environ.get('PORT', '8000')}"
+workers = 4
+threads = 4
+timeout = 120
